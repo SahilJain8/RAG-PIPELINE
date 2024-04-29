26045b1065d3bd5eacefc5cccfe7005741b816a6
Sahil <68115080+SahilJain8@users.noreply.github.com>
2024-04-29 12:15:40 +0530

    added command line interface for -> main.py

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 from rag_loading_data import Dataset_load
 from rag_models import Rag_Model
+import argparse
+import time
+import torch
 
-dataset_load = Dataset_load(df_path="text_chunks_and_embeddings_df.csv")
-dataset_load.save_embeddings()
-rag_model = Rag_Model()
 
-print("[INFO] Enter your query")
-query = str(input())
-query_embeddings = rag_model.embedding_model.encode(query)
-scores, samples = dataset_load.retun_faiss_index(query_embeddings)
-prompt = rag_model.create_prompt(samples['sentence_chunk'], query)
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"[INFO] using {device} as the device")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='google/gemma-2b-it')
+    parser.add_argument('--encoding_model_name', type=str, default='all-mpnet-base-v2')
+    parser.add_argument('--use_quantization', type=bool, default=True)
+    args = parser.parse_args()
+    embedding_model_name = args.encoding_model_name
+    model_name = args.model_name
+    use_quantization = args.use_quantization
+    print("[INFO] Encoding Model: {} \n[INFO] LLM model: {} \n[INFO] Quantisation is set to: {}".format(
+        embedding_model_name,
+        model_name,
+        use_quantization))
+    start_time = time.time()
+    dataset_load = Dataset_load(df_path="text_chunks_and_embeddings_df.csv",
+                                device=device)
+    dataset_load.save_embeddings()
+    rag_model = Rag_Model(model_id=model_name,
+                          embedding_model_name=embedding_model_name,
+                          use_quantization=use_quantization,
+                          device=device)
+    end_time = time.time()
 
-ans = rag_model.generate(prompt)
+    print(f"[INFO] Runtime: {round(end_time - start_time, 2)} seconds")
 
-print(ans)
+    while True:
+        start_time = time.time()
+        print("[INFO] Enter your query")
+        query = str(input())
+        query_embeddings = rag_model.embedding_model.encode(query)
+        scores, samples = dataset_load.retun_faiss_index(query_embeddings)
+        prompt = rag_model.create_prompt(samples['sentence_chunk'], query)
 
+        ans = rag_model.generate(prompt)
+
+        print(ans.replace("<s>", " ").replace("</s>", " "))
+        end_time = time.time()
+        print(f"[INFO] Runtime: {round(end_time - start_time, 2)} seconds")
+
+
+if __name__ == '__main__':
+    main()
--- rag_loading_data.py
+++ rag_loading_data.py
@@ -1,16 +1,14 @@
 import torch
 import pandas as pd
 import numpy as np
-import time
 from datasets import Dataset
 
 
 class Dataset_load:
-    def __init__(self, df_path: str):
+    def __init__(self, df_path: str, device: str = 'cpu'):
         self.embeddings_dataset = None
-        start = time.time()
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"[INFO] using {self.device} device")
+
+        self.device = device
         self.df_path = df_path
         self.text_and_embeddings_df = pd.read_csv(self.df_path)
         print(f"[INFO] Loaded the Dataset from {self.df_path}")
@@ -20,8 +18,6 @@ class Dataset_load:
         self.embeddings = torch.tensor(np.array(self.text_and_embeddings_df['embedding'].tolist()),
                                        dtype=torch.float32).to(self.device)
         print(f"[INFO] embedding shape {self.embeddings.shape}")
-        end = time.time()
-        print("[INFO] Elapsed time: {:.2f}".format(end - start))
 
     def save_embeddings(self):
         self.embeddings_dataset = Dataset.from_pandas(self.text_and_embeddings_df)
